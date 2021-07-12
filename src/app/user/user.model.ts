7415f8acce513ee407fa0d57c0157556b447fa8c
Firouzeh-Dabbaghi <70266008+Firouzeh-Dabbaghi@users.noreply.github.com>
2021-07-12 18:15:41 +0430

    add user model

---
+++ src/app/user/user.model.ts
@@ -0,0 +1,11 @@
+export class User {
+    constructor(
+        public name: string,
+        public family: string,
+        public email: string,
+        public birthDate: string,
+        public address: string,
+        public postalCode: string
+    ) {
+    }
+}
