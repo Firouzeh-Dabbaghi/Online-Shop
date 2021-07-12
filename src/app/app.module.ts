914ce40403a3505591467f043694ac5318e24640
Firouzeh-Dabbaghi <70266008+Firouzeh-Dabbaghi@users.noreply.github.com>
2021-07-12 18:18:46 +0430

    add routing and component  etc to appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,20 @@
-import { RouterModule, Routes } from '@angular/router';
-
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { CarouselComponent } from './shared/carousel/carousel.component';
 import { ContentComponent } from './content/content.component';
 import { FontAwesomeDirective } from 'ng2-fontawesome';
 import { Footer } from './global/footer/footer.component';
+import { FormsModule } from '@angular/forms';
 import { LightboxComponent } from './shared/lightbox/lightbox.component';
 import { LightboxModule } from 'ngx-lightbox';
 import { NgModule } from '@angular/core';
 import { NoContentComponent } from './global/no-content/no-content.component';
 import { ProductsService } from './products/products.service';
 import { SidebarComponent } from './menu/sidebar/sidebar.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
 import { SlideshowModule } from 'ng-simple-slideshow';
 import { TopNavBarComponent } from './menu/top-nav-bar/top-nav-bar.component';
-
-export const AppRoutes2: Routes = [
-  { path: '', component: ContentComponent },
-  /*{ path: 'inbox',  component: Inbox },
-  { path: 'about', component: About },*/
-  { path: '**', component: NoContentComponent }
-];
 
 @NgModule({
   declarations: [
@@ -32,13 +26,15 @@ export const AppRoutes2: Routes = [
     FontAwesomeDirective,
     Footer,
     CarouselComponent,
-    LightboxComponent
+    LightboxComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(AppRoutes2, { useHash: true }),
+    FormsModule,
     SlideshowModule,
-    LightboxModule ],
+    LightboxModule,
+    AppRoutingModule],
   providers: [ProductsService],
   bootstrap: [AppComponent]
 })
