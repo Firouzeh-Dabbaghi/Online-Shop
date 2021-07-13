9a8bae2196e4412db0ce54d176a108ae733a7333
Firouzeh-Dabbaghi <70266008+Firouzeh-Dabbaghi@users.noreply.github.com>
2021-07-13 15:31:14 +0430

    when params of route change can detect it

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,7 @@
+import { ActivatedRoute, Params } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-import { User, UserMode } from './user.model';
+import { User, UserPageState } from './user.model';
 
-import { ActivatedRoute } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { UserService } from './user.service';
 
@@ -11,24 +11,31 @@ import { UserService } from './user.service';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-  user: User;
-  mode = UserMode.SignUp;
-  userMode=UserMode
+  user = new User(null, '', '', '', '', '', null);
+  pageState = UserPageState.SignUp;
+  userMode = UserPageState
   constructor(
     private route: ActivatedRoute,
     private userService: UserService) { }
 
   ngOnInit() {
     const id = +this.route.snapshot.params['id'];
-    const edit = +this.route.snapshot.params['edit'];
-
+    this.pageState = this.route.snapshot.params['pageState'];
     if (id > 0) {
       this.user = this.userService.getuser(id);
-      if (edit)
-        this.mode = UserMode.Edit;
-      else
-        this.mode = UserMode.ReadOnly
     }
+
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.getParamsRoute(params);
+        }
+      );
+  }
+
+  private getParamsRoute(params: Params) {
+    this.user = this.userService.getuser(+params['id']);
+    this.pageState = this.route.snapshot.params['pageState'];
   }
 
   onAddUser(form: NgForm) {
