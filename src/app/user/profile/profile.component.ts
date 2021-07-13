16bd4d5dcce5d5c84d1e4ccf194bf7865e545c56
Firouzeh-Dabbaghi <70266008+Firouzeh-Dabbaghi@users.noreply.github.com>
2021-07-13 15:31:58 +0430

    use routerLinkActive and set rout link

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserPageState } from './../user.model';
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
+  pageState = UserPageState
   constructor() { }
 
   ngOnInit() {
